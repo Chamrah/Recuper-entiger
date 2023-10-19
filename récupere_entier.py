a20cdb04230f9464286bfc3b39cd81b94c5d5d63
Ziad Chamrah <147912275+Chamrah@users.noreply.github.com>
2023-10-19 07:39:38 +0100

    Add files via upload

---
+++ récupere_entier.py
@@ -0,0 +1,33 @@
+#  Programme retrieves the grades of a class. Subsequently, the algorithm must display grades above average.
+n = int(input("Entrez un entier n : "))
+p = []
+for num in range(2, n):
+    
+    is_p = True
+  
+    for i in range(2, num):
+        if num % i == 0:
+            is_p = False
+            break  
+    
+    
+   
+p.append(num)
+
+
+print(f"Les nombres premiers inférieurs à {n} sont : {p}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
